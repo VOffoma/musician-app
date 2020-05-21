e44c72fc30f3d3e43bab8bc53fa772d78dd188db
Victoria Offoma <offoma.victoria@gmail.com>
2020-05-21 17:20:39 +0100

    update hero section

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
-          Musician App
+          Musician App for all music lovers
         </div>
       </div>
     </section>
